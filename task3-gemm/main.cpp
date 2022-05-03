ad5d466767bf154b714bf4b1cd08c1cdd2c0db46
EAirPeter <VioletCrestfall@hotmail.com>
2022-05-03 19:54:55 +0800

    Task 3: Add ASM baseline (5.3x,4.5x,1.5x,1.3x)

--- task3-gemm/main.cpp
+++ task3-gemm/main.cpp
@@ -24,8 +24,81 @@ using vec = vector<int>;
 const int scale[] = {256, 512, 1024, 2048};
 const string data_path("./data/");
 
+#define USE_ASM 1
+
+#if USE_ASM
+__attribute__((naked))
+static void GemmImpl(int N, const int* A, const int* B, int* C)
+{
+    __asm__ volatile (R"???(
+        .intel_syntax noprefix
+
+        .equiv N, edi
+        .equiv Nq, rdi
+        .equiv InA, rsi
+        .equiv InB, rdx
+        .equiv OutC, rcx
+        .equiv A, r8
+        .equiv B, r9
+        .equiv C, r10
+        .equiv i, ecx
+        .equiv j, esi
+        .equiv jq, rsi
+        .equiv k, ebx
+        .equiv kq, rbx
+        .equiv Sum, eax
+        .equiv CurB, r11
+        .equiv Tmp, edx
+
+        test N, N
+        jz .L.ExitNoFrame
+
+        push rbx
+
+        mov A, InA
+        mov B, InB
+        mov C, OutC
+
+        mov i, N
+        shl N, 2
+    .L.LoopI:
+        xor j, j
+        .L.LoopJ:
+            xor Sum, Sum
+            xor k, k
+            lea CurB, [B + jq]
+            .L.LoopK:
+                mov Tmp, dword ptr [A + kq]
+                imul Tmp, dword ptr [CurB]
+                add Sum, Tmp
+                add k, 4
+                add CurB, Nq
+                cmp k, N
+                jne .L.LoopK
+            mov dword ptr [C + jq], Sum
+            add j, 4
+            cmp j, N
+            jne .L.LoopJ
+        add A, Nq
+        add C, Nq
+        sub i, 1
+        jnz .L.LoopI
+
+    .L.Exit:
+        pop rbx
+    .L.ExitNoFrame:
+        ret
+
+        .att_syntax
+    )???");
+}
+#endif
+
 void Gemm(const int &inSize, vec &inA, vec &inB, vec &outC)
 {
+#if USE_ASM
+    GemmImpl(inSize, inA.data(), inB.data(), outC.data());
+#else
     register const int size = inSize;
     register const int* a = inA.data();
     register const int* b = inB.data();
@@ -40,6 +113,7 @@ void Gemm(const int &inSize, vec &inA, vec &inB, vec &outC)
             c[i * size + j] = sum;
         }
     }
+#endif
 }
 
 void CheckResult(const vec &c, const string &result_path) {
